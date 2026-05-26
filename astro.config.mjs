0a0ac1921c1d93eed3b6570947cdecd877ce3626
Hakimov <naim.nanocoding@gmail.com>
2026-05-26 16:11:18 +0500

    Integrate sitemap plugin, update site URL, and add Google site verification files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://178.105.17.88',
-  integrations: [react(), icon()],
+  site: 'https://pankartin.vercel.app',
+  integrations: [react(), icon(), sitemap()],
 });
